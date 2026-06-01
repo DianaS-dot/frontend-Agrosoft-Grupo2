53719a13d15b26d176673f34015e98e3feaa90ac
DianaS-dot <scarpetapenad@gmail.com>
2026-05-31 20:26:33 -0500

    pantallas de agrosoft ya contadas/

---
+++ recursos/javascrip/infocultivo.js
@@ -0,0 +1,11 @@
+// control del botón EDITAR CULTIVO 
+const botonEditar = document.getElementById("btn-editar");
+botonEditar.addEventListener("click", function() {
+    alert("Abriendo el editor del cultivo de lechuga...");
+});
+
+// CONTROL DEL BOTÓN REGISTRAR INCIDENCIA (ARRIBA)
+const botonIncidencia = document.getElementById("btn-incidencia");
+botonIncidencia.addEventListener("click", function() {
+    alert("Cargando el formulario de incidencias...");
+});
---
+++ recursos/javascrip/siembracosecha.js
@@ -0,0 +1,7 @@
+// CONTROL DEL BOTÓN REGISTRAR COSECHA
+const botonRegistrar = document.getElementById("btn-registrar-cosecha");
+
+botonRegistrar.addEventListener("click", function() {
+    // Aquí puedes cambiar la alerta por la acción real que necesites después
+    alert("Procesando: Abriendo formulario para el registro definitivo de la cosecha de lechugas.");
+});
