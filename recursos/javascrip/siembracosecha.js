2c5f251ef8afcefe7e8cc1053f596096163b7cc4
DianaS-dot <scarpetapenad@gmail.com>
2026-06-03 13:43:37 -0500

    cambios en js

--- recursos/javascrip/siembracosecha.js
+++
@@ -1,7 +0,0 @@
-// CONTROL DEL BOTÓN REGISTRAR COSECHA
-const botonRegistrar = document.getElementById("btn-registrar-cosecha");
-
-botonRegistrar.addEventListener("click", function() {
-    // Aquí puedes cambiar la alerta por la acción real que necesites después
-    alert("Procesando: Abriendo formulario para el registro definitivo de la cosecha de lechugas.");
-});
